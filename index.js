37ec8c0e23a28f7db71cf60a3ea075ab3ae4142b
Mohammad-Faisal <mohammadfaisal@gmail.com>
2020-05-03 01:32:55 +0600

    donw using operation module

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
 const MongoClient = require('mongodb').MongoClient;
-
 const assert = require ('assert');
-
 const url = 'mongodb://localhost:27017';
 const dbName = 'confusion';
+const dboper = require('./operations');
+
+
 MongoClient.connect(url , (err , client) => {
    assert.equal(err , null);
 
    console.log('connected correctly ');
    const db   = client.db(dbName);
    const collecion = db.collection('dishes');
-   collecion.insertOne({"name" :"second object" , "description" :"test description"} , (err ,result) => {
-       assert.equal(err , null);
-       console.log("after insert  " ,result.ops);
 
-       collecion.find({}).toArray((err , docs) => {
-           console.log("found this documents " , docs);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+        "dishes", (result) => {
+            console.log("Insert Document:\n", result.ops);
 
+            dboper.findDocuments(db, "dishes", (docs) => {
+                console.log("Found Documents:\n", docs);
 
-       });
+                dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes",
+                    (result) => {
+                        console.log("Updated Document:\n", result.result);
 
-       // db.dropCollection('dishes' , (err , result) => {
-       //     assert.equal(err , null);
-       //
-       //     client.close();
-       // })
-   });
+                        dboper.findDocuments(db, "dishes", (docs) => {
+                            console.log("Found Updated Documents:\n", docs);
+
+                            db.dropCollection("dishes", (result) => {
+                                console.log("Dropped Collection: ", result);
+
+                                client.close();
+                            });
+                        });
+                    });
+            });
+        });
+
+   // collecion.insertOne({"name" :"second object" , "description" :"test description"} , (err ,result) => {
+   //     assert.equal(err , null);
+   //     console.log("after insert  " ,result.ops);
+   //
+   //     collecion.find({}).toArray((err , docs) => {
+   //         console.log("found this documents " , docs);
+   //
+   //
+   //     });
+   //
+   //     // db.dropCollection('dishes' , (err , result) => {
+   //     //     assert.equal(err , null);
+   //     //
+   //     //     client.close();
+   //     // })
+   // });
 });
